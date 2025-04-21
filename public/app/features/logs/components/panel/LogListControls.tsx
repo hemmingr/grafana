6dfa750477c6e647037e6cd051c2fb88b6f21fdb
Matias Chomicki <matyax@gmail.com>
2025-04-21 18:45:48 +0200

    LogListControls: disallow unique labels in apps

--- public/app/features/logs/components/panel/LogListControls.tsx
+++ public/app/features/logs/components/panel/LogListControls.tsx
@@ -33,7 +33,6 @@ const FILTER_LEVELS: LogLevel[] = [
   LogLevel.warning,
   LogLevel.error,
   LogLevel.critical,
-  LogLevel.unknown,
 ];
 
 export const LogListControls = ({ eventBus, visualisationType = 'logs' }: Props) => {
@@ -265,7 +264,6 @@ export const LogListControls = ({ eventBus, visualisationType = 'logs' }: Props)
                 }
                 size="lg"
               />
-              {/* When this is used in a Plugin context, app is unknown */}
               {showUniqueLabels !== undefined && app !== CoreApp.Unknown && (
                 <IconButton
                   name="tag-alt"
@@ -346,6 +344,7 @@ export const LogListControls = ({ eventBus, visualisationType = 'logs' }: Props)
                 <IconButton
                   name="download-alt"
                   className={styles.controlButton}
+                  aria-pressed={wrapLogMessage}
                   tooltip={t('logs.logs-controls.download', 'Download logs')}
                   size="lg"
                 />
@@ -390,7 +389,6 @@ const getStyles = (theme: GrafanaTheme2) => {
       paddingTop: theme.spacing(0.75),
       paddingLeft: theme.spacing(1),
       borderLeft: `solid 1px ${theme.colors.border.medium}`,
-      overflow: 'hidden',
     }),
     scrollToTopButton: css({
       margin: 0,
